4a220714873de3be5ca55ab9939d682b5d42eebf
AlekseiDanilov <danilow.aleksei@gmail.com>
2019-01-03 21:51:37 +0300

    fix migrations

--- server/migrations/000_000_001.js
+++ server/migrations/000_000_001.js
@@ -1,5 +1,5 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.withSchema()
+exports.up = async function(knex, Promise) {
+  await knex.schema.withSchema()
     .createTable('user', function(t) {
       t.string('id', 32).primary();
       t.string('name', 100);
@@ -7,6 +7,7 @@ exports.up = function(knex, Promise) {
       t.string('username', 100).notNullable();
       t.text('passwordHash').notNullable();
     });
+  await knex.raw("INSERT INTO \"user\" VALUES ('0', 'Admin', 'admin@lol.net', 'admin', md5('qwer1234'))");
 };
 
 exports.down = function(knex, Promise) {
--- server/migrations/000_000_002.js
+++
@@ -1,8 +0,0 @@
-
-exports.up = function(knex, Promise) {
-  return knex.raw("INSERT INTO \"user\" VALUES ('0', 'Admin', 'admin@lol.net', 'admin', md5('qwer1234'))");
-};
-
-exports.down = function(knex, Promise) {
-  return knex('user').where({id: "0"}).del();
-};
