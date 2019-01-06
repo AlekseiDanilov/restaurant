fa85bac9a51d52912e6a4f0957a3d0aa143d9e26
AlekseiDanilov <danilow.aleksei@gmail.com>
2019-01-06 19:36:25 +0300

    ssl for test server

--- frontend/staticserver/frontend.js
+++ frontend/staticserver/frontend.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const proxy = require('http-proxy-middleware');
-app.use(express.static(path.join(__dirname, '../build')));
+
+const https = require('https');
+const pem = require('pem');
+const enableHttps = process.env.ENABLE_HTTPS;
+const http = require('http');
+const app = express();
 
 const backendPort = process.env.BACKEND_PORT || 3001;
 const backendHost = process.env.BACKEND_HOST || 'http://localhost';
 const backendServerProxy = proxy({
-    target: backendHost + ':' + backendPort,
-    changeOrigin: false,
-    pathRewrite: { '^/api' : '/api' },
-    autoRewrite: true
-  });
-app.use( '/api/*', backendServerProxy );
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  target: backendHost + ':' + backendPort,
+  changeOrigin: false,
+  pathRewrite: {'^/api': '/api'},
+  autoRewrite: true
 });
 
-app.listen(80);
+if (enableHttps) {
+  pem.createCertificate({days: 7, selfSigned: true}, function (err, opts) {
+    if (err) {
+      throw err
+    }
+    https.createServer({key: opts.serviceKey, cert: opts.certificate}, app).listen(443)
+  });
+}
+http.createServer(app).listen(80);
+
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/api/*', backendServerProxy);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
